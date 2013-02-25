af0929b901e78c94257f598f517517508e66efb3
yannicklm <yannicklm1337@gmail.com>
2013-02-25 22:41:57 +0100

    backend: use git when possible

--- pyfortunes/backend.py
+++ pyfortunes/backend.py
@@ -7,6 +7,7 @@ requests
 """
 
 import os
+import subprocess
 import random
 
 class FortunesDB():
@@ -20,25 +21,45 @@ class FortunesDB():
     """
     def __init__(self, directory):
         self.directory = directory
+        self.using_git = os.path.exists(os.path.join(directory, ".git"))
 
     def get_categories(self):
         res = [x for x in os.listdir(self.directory) if not x.endswith(".dat")]
         res.sort()
         return res
 
+    def git_push(self, category):
+        """ Push the modification to git """
+        subprocess.check_call(["git", "add", category],
+                              cwd=self.directory)
+        subprocess.check_call(["git", "commit", "-m", "update %s" % category],
+                              cwd=self.directory)
+        subprocess.check_call(["git", "push"],
+                               cwd=self.directory)
+
+    def git_pull(self):
+        """ Run git pull """
+        subprocess.check_call(["git", "pull", "--rebase"],
+                              cwd=self.directory)
+
     def add_fortune(self, category, text):
+        """ Add a new fortune """
         if not category:
             raise Exception("category cannot be empty")
         if not text:
             raise Exception("text cannot be empty")
         if not text.endswith("\n"):
             text += "\n"
+        if self.using_git:
+            self.git_pull()
         filename = os.path.join(self.directory, category)
         with open(filename, "a", encoding='utf-8') as fp:
             fp.write("%\n")
             fp.write(text)
-
+        if self.using_git:
+            self.git_push(category)
         return True
+
     def get_fortune(self, category=None):
         """ Get a fortune.
         If category is not None, only use fortunes from the
