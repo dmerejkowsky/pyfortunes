4df253c1c4b4d69575fc43d77459cb9349ee7b1d
Dimitri Merejkowsky <d.merej@gmail.com>
2013-09-08 14:09:25 +0200

    pydtc: changes for the new site

--- contrib/pydtc.py
+++ contrib/pydtc.py
@@ -14,29 +14,36 @@ def get_quote(id):
     url = "http://danstonchat.com/%s.html" % id
     response = requests.get(url)
     html = response.text
+    return extract_quote(html, id)
+
+def extract_quote(html, id):
     soup = BeautifulSoup(html)
+    url = "http://danstonchat.com/%s.html" % id
     # The quote is in the first <a> link that points to
     # self url ...
-    self_links = soup("a", href="/%s.html" % id)
+    self_links = soup("a", href=url)
     if not self_links:
-        print("no quote found?")
         return
     res = ""
     quote = self_links[0]
     # Quote is: <span class > for the pseudo, then some text, then <br />
     # Get only the pseudo, but keep the br and wrap the quote at
     # 80
+    res = ""
     reply = ""
     for child in quote.children:
-        if hasattr(child, "name"):
-            if child.name == "span":
-                reply += child.text
-            if child.name == "br":
-                reply = ""
-                res += "\n"
-        else:
-            res += "\n".join(textwrap.wrap(reply + child))
-    res += "\n"
+        if not child.name:
+            reply += child
+        if child.name == "span":
+            reply = child.text
+        if child.name == "br":
+            res += "\n".join(textwrap.wrap(reply))
+            res += "\n"
+            reply = ""
+    # there's no final <br />
+    if reply:
+        res += "\n".join(textwrap.wrap(reply))
+        res += "\n"
     return res
 
 
@@ -48,6 +55,8 @@ def main():
     ns = argparse.Namespace(url=None)
     srv_url = pyfortunes.client.get_url(ns)
     quote = get_quote(url)
+    if not quote:
+        sys.exit("no quote found")
     proxy = pyfortunes.client.get_proxy(srv_url)
     pyfortunes.client.add_fortune(proxy, category="bash", text=quote)
 
